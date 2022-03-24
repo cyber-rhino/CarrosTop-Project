1b18512d0f9f4e3c7534dead6c51b3d46fce97bc
Franciscocarlosinfo2020@gmail.com <joaquimferreira1818@gmail.com>
2022-03-24 20:54:54 -0300

    ajuste catalogo

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -4,28 +4,28 @@ function Catalog() {
   return (
       <div className="container my-4">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 ">
           <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 ">
           <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 ">
           <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 ">
           <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 ">
           <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 ">
           <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 ">
           <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 ">
           <ProductCard />
           </div>
         </div>
