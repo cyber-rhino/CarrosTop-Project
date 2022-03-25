00b3e87ab4bc0296739a04982c2ed02e4f670638
Franciscocarlosinfo2020@gmail.com <joaquimferreira1818@gmail.com>
2022-03-25 11:09:29 -0300

    add do botão de pesquisa

---
+++ src/components/ButtonSearch/index.tsx
@@ -0,0 +1,23 @@
+import './styles.css';
+
+    function ButtonSearch() {
+
+        return(
+        
+        <div className="btn btn-card second-card-forma container">
+            <div className="btn-1">
+                <button>
+                    <h5>Digite sua busca</h5>
+                </button>
+            </div>
+
+            <div className="btn-2">
+                <button>
+                    <h5>Buscar</h5>
+                </button>
+            </div>
+        </div>    
+        );
+    }
+
+export default ButtonSearch;
--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,9 +1,14 @@
 import ProductCard from "components/ProductCard";
+import ButtonSearch from "components/ButtonSearch";
 
 function Catalog() {
   return (
       <div className="container my-4">
         <div className="row">
+          <div>
+            <ButtonSearch />
+          </div>
+
           <div className="col-sm-6 col-lg-4">
           <ProductCard />
           </div>
